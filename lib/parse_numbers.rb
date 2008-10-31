c8c4a65d8b74ce5d7c7f4520cadb35b2a241f42d
Bruno Pedroso <Bruno@macasa.local>
2008-10-31 21:36:18 -0200

    refatorei mais um pouco

--- lib/parse_numbers.rb
+++ lib/parse_numbers.rb
@@ -9,23 +9,28 @@ class DisplayParser
 				4
 			end
 		
-		elsif digits[1] == "|_ "
+		else
 			
-			if digits[2] == "|_|"
-				6
-			else
-				5
+			if digits[1] == "|_ "
+			
+				if digits[2] == "|_|"
+					6
+				else
+					5
+				end
+			
+	    else
+	
+				if digits[2] == "|_ "
+	      	2
+				else
+					3
+	    	end
+
 			end
 			
-    else
-	
-			if digits[2] == "|_ "
-      	2
-			else
-				3
-    	end
-
 		end
+		
   end
   
 end
