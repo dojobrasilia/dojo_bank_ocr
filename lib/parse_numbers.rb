d594e9939061e43b04262a154bf4c140798e5175
Bruno Pedroso <Bruno@macasa.local>
2008-10-31 21:23:55 -0200

    refatorei um pouco

--- lib/parse_numbers.rb
+++ lib/parse_numbers.rb
@@ -2,19 +2,25 @@ class DisplayParser
   
   def self.parse(digits)
     if digits[0] == "   "
+			
 			if digits[1] == "  |"
       	1
 			else
 				4
 			end
 		
-    elsif digits[2] == "|_ "
-      2
 		elsif digits[1] == "|_ "
 			5
-		else
-			3
-    end
+			
+    else
+	
+			if digits[2] == "|_ "
+      	2
+			else
+				3
+    	end
+
+		end
   end
   
 end
