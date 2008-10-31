e843ada3a9e6da9d29bf44a7d89b994a0a6222d5
Bruno Pedroso <Bruno@macasa.local>
2008-10-31 21:49:34 -0200

    refatorei mais um pouco os if's e case's

--- lib/parse_numbers.rb
+++ lib/parse_numbers.rb
@@ -1,32 +1,33 @@
 class DisplayParser
   
   def self.parse(digits)
-    if digits[0] == "   "
-			
-			case
+	
+		if digits[0] == "   " 
+
+			case 
 				when digits[1] == "  |" then 1
 				else 4
 			end
-		
+
 		else
 			
 			if digits[1] == "|_ "
-			
-				case
+				
+				case 
 					when digits[2] == "|_|" then	6
 					else 5
 				end
-			
-	    else
-	
+				
+			else
+					
 				case
-				when digits[2] == "|_ " then 2
-				when digits[2] == " _|" then 3
-				else 7
+					when digits[2] == "|_ " then 2
+					when digits[2] == " _|" then 3
+					else 7
 				end	
-
+					
 			end
-			
+				
 		end
 		
   end
